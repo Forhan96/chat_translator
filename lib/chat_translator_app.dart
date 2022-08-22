7928c437742536a70654b8ce47e48d4bca00f779
Forhan96 <f.a.chowdhury19@gmail.com>
2022-08-23 01:01:39 +0600

    - Login done - Logout done

--- lib/chat_translator_app.dart
+++ lib/chat_translator_app.dart
@@ -1,7 +1,11 @@
 import 'package:chat_translator/router/router_helper.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:chat_translator/screens/home_screen.dart';
+import 'package:chat_translator/screens/sign_in_screen.dart';
+import 'package:chat_translator/services/authentication_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 class ChatTranslatorApp extends StatefulWidget {
   const ChatTranslatorApp({Key? key}) : super(key: key);
@@ -22,14 +26,39 @@ class _ChatTranslatorAppState extends State<ChatTranslatorApp> {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(414, 896),
-      builder: (context, child) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter_ScreenUtil',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      builder: (context, child) => MultiProvider(
+        providers: [
+          Provider<AuthenticationService>(
+            create: (_) => AuthenticationService(FirebaseAuth.instance),
+          ),
+          StreamProvider(
+            create: (context) => context.read<AuthenticationService>().authStateChanges,
+            initialData: null,
+          )
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter_ScreenUtil',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const AuthenticationWrapper(),
         ),
-        home: HomePage(),
       ),
     );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User?>();
+
+    if (firebaseUser != null) {
+      return HomeScreen();
+    }
+    return SignInScreen();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:chat_translator/chat_translator_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const ChatTranslatorApp());
 }
